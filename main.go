ba09aca29104c1514e6eda590da292868f7f252b
Giulio Iotti <dullgiulio@gmail.com>
2016-05-11 11:50:53 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+type dumptransport struct {}
+
+func (t *dumptransport) RoundTrip(request *http.Request) (*http.Response, error) {
+    response, err := http.DefaultTransport.RoundTrip(request)
+    dump, err := httputil.DumpResponse(response, true)
+    if err != nil {
+        return nil, err
+    }
+	fmt.Printf("%s\n\n", dump)
+    return response, err
+}
+
+func main() {
+	listen := flag.String("listen", ":8888", "`HOST:PORT` to listen to")
+	to := flag.String("to", "reverse.proxy", "`HOST` to reverse proxy")
+	flag.Parse()
+	director := func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = *to
+		dump, err := httputil.DumpRequestOut(req, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n\n", dump)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy := &httputil.ReverseProxy{Director: director}
+		proxy.Transport = &dumptransport{}
+		proxy.ServeHTTP(w, r)
+	})
+	log.Fatal(http.ListenAndServe(*listen, nil))
+}
